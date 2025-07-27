496ea770279b9d4cb39c54f5c0e02c9580e90193
Collin Dietz <collin.dietz@geappliances.com>
2025-07-26 20:49:52 -0400

    Basic MVP, help from ChatGPT

---
+++ lib/data_loader.dart
@@ -0,0 +1,10 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'models/nail_polish.dart';
+
+Future<List<NailPolish>> loadNailPolishData() async {
+  final String jsonString =
+      await rootBundle.loadString('data/gel_polishes.json');
+  final List<dynamic> jsonData = json.decode(jsonString);
+  return jsonData.map((item) => NailPolish.fromJson(item)).toList();
+}
